4539fcc6c7a44126b6dbac7dd21f1d8b3c39a5b2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-13 05:02:32 +0000

    Deploy liquality/liquality-swap to github.com/liquality/liquality-swap.git:gh-pages

---
+++ precache-manifest.e9d286466288d1a17fdd093b30abc308.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "4cfa0cb4e818a1db9db83ec88aba80b6",
+    "url": "./static/media/ledger.4cfa0cb4.svg"
+  },
+  {
+    "revision": "ef7f613125b25ded1d09",
+    "url": "./static/css/main.18cee59e.chunk.css"
+  },
+  {
+    "revision": "af6ea3a207520fce9ae8",
+    "url": "./static/js/1.af6ea3a2.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "c5ff69206d38da7edfcf98b528d32659",
+    "url": "./static/media/completed.c5ff6920.svg"
+  },
+  {
+    "revision": "26f38b8334d2577496707a9f3c47b203",
+    "url": "./static/media/copy.26f38b83.svg"
+  },
+  {
+    "revision": "baea04ec9372dbacd6b81d2bdb25ba2a",
+    "url": "./static/media/clock.baea04ec.svg"
+  },
+  {
+    "revision": "35e4ba88ae49faf49e433ddc516d8315",
+    "url": "./static/media/handshake.35e4ba88.png"
+  },
+  {
+    "revision": "e73cb1b222d746763a3d57ab92c94621",
+    "url": "./static/media/btc.e73cb1b2.svg"
+  },
+  {
+    "revision": "18708d3e3f54116e2ea639fdd56dd6c5",
+    "url": "./static/media/eth.18708d3e.svg"
+  },
+  {
+    "revision": "dcf1fb0e8d0f139a689db6fb4a51d163",
+    "url": "./static/media/metamask.dcf1fb0e.svg"
+  },
+  {
+    "revision": "ef7f613125b25ded1d09",
+    "url": "./static/js/main.ef7f6131.chunk.js"
+  },
+  {
+    "revision": "db956da8eeb26f16a9af0bfc73f666d9",
+    "url": "./static/media/ledger_purple.db956da8.svg"
+  },
+  {
+    "revision": "1755dc6e11b57d4d7b7e1aa113f1d848",
+    "url": "./static/media/wallet-icon.1755dc6e.svg"
+  },
+  {
+    "revision": "df3317e43b14772987fc5a887c490f9c",
+    "url": "./static/media/assets-bg.df3317e4.svg"
+  },
+  {
+    "revision": "91b58e9eb410cb4b37b381a4fcd61759",
+    "url": "./static/media/tick.91b58e9e.svg"
+  },
+  {
+    "revision": "9c76fef58cf4c5fca97dfe6b019a90a1",
+    "url": "./static/media/error.9c76fef5.svg"
+  },
+  {
+    "revision": "6b34fdcbf274423844143e39b42e4269",
+    "url": "./static/media/lock.6b34fdcb.svg"
+  },
+  {
+    "revision": "31f6da16ad75875446a21db692efdf64",
+    "url": "./static/media/switch.31f6da16.svg"
+  },
+  {
+    "revision": "f8b51b82a870c299a592fc57be0aacb4",
+    "url": "./static/media/logo.f8b51b82.png"
+  },
+  {
+    "revision": "3c2c333802ceffc62537a6971ea56d0a",
+    "url": "./static/media/logo-text.3c2c3338.png"
+  },
+  {
+    "revision": "fa9bd08a94667f9c4c0a60bd4fcb1e42",
+    "url": "./index.html"
+  }
+];
